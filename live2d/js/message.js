bf0e7d51c724b44c8a844c6458ceaacd69c3c45a
No.5972 <32865305+No5972@users.noreply.github.com>
2020-07-10 11:20:12 +0800

    Update message.js

--- live2d/js/message.js
+++ live2d/js/message.js
@@ -59,45 +59,30 @@ function initTips(){
 initTips();
 
 (function (){
-    var text;
-    if(document.referrer !== ''){
-        var referrer = document.createElement('a');
-        referrer.href = document.referrer;
-        text = '嗨！来自 <span style="color:#0099cc;">' + referrer.hostname + '</span> 的朋友！';
-        var domain = referrer.hostname.split('.')[1];
-        if (domain == 'baidu') {
-            text = '嗨！ 来自 百度搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
-        }else if (domain == 'so') {
-            text = '嗨！ 来自 360搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
-        }else if (domain == 'google') {
-            text = '嗨！ 来自 谷歌搜索 的朋友！<br>欢迎访问<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
-        }
-    }else {
-        if (window.location.href == `${home_Path}`) { //主页URL判断，需要斜杠结尾
-            var now = (new Date()).getHours();
-            if (now > 23 || now <= 5) {
-                text = '你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？';
-            } else if (now > 5 && now <= 7) {
-                text = '早上好！一日之计在于晨，美好的一天就要开始了！';
-            } else if (now > 7 && now <= 11) {
-                text = '上午好！工作顺利嘛，不要久坐，多起来走动走动哦！';
-            } else if (now > 11 && now <= 14) {
-                text = '中午了，工作了一个上午，现在是午餐时间！';
-            } else if (now > 14 && now <= 17) {
-                text = '午后很容易犯困呢，今天的运动目标完成了吗？';
-            } else if (now > 17 && now <= 19) {
-                text = '傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红~~';
-            } else if (now > 19 && now <= 21) {
-                text = '晚上好，今天过得怎么样？';
-            } else if (now > 21 && now <= 23) {
-                text = '已经这么晚了呀，早点休息吧，晚安~~';
-            } else {
-                text = '嗨~ 快来逗我玩吧！';
-            }
-        }else {
-            text = '欢迎阅读<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
-        }
+    let text;
+    if (location.pathname === "/" || location.pathname === "/cms/") { // 如果是主页
+        let now = new Date().getHours();
+        if (now > 5 && now <= 7) text = "早上好！一日之计在于晨，美好的一天就要开始了。";
+        else if (now > 7 && now <= 11) text = "上午好！工作顺利嘛，不要久坐，多起来走动走动哦！";
+        else if (now > 11 && now <= 13) text = "中午了，工作了一个上午，现在是午餐时间！";
+        else if (now > 13 && now <= 17) text = "午后很容易犯困呢，今天的运动目标完成了吗？";
+        else if (now > 17 && now <= 19) text = "傍晚了！窗外夕阳的景色很美丽呢，最美不过夕阳红～";
+        else if (now > 19 && now <= 21) text = "晚上好，今天过得怎么样？";
+        else if (now > 21 && now <= 23) text = ["已经这么晚了呀，早点休息吧，晚安～", "深夜时要爱护眼睛呀！"];
+        else text = "你是夜猫子呀？这么晚还不睡觉，明天起的来嘛？";
+    } else if (document.referrer !== "") {
+        let referrer = new URL(document.referrer),
+            domain = referrer.hostname.split(".")[1];
+        if (location.hostname === referrer.hostname) text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
+        else if (domain === "baidu") text = `Hello！来自 百度搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&wd=")[1].split("&")[0]}</span> 找到的我吗？`;
+        else if (domain === "so") text = `Hello！来自 360搜索 的朋友<br>你是搜索 <span>${referrer.search.split("&q=")[1].split("&")[0]}</span> 找到的我吗？`;
+        else if (domain === "google") text = `Hello！来自 谷歌搜索 的朋友<br>欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
+        else if (domain === "bing") text = `Hello！来自 必应搜索 的朋友<br>欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
+        else text = `Hello！来自 <span>${referrer.hostname}</span> 的朋友`;
+    } else {
+        text = `欢迎阅读<span>「${document.title.split(" - ")[0]}」</span>`;
     }
+
     showMessage(text, 12000);
 })();
 
